cbc3940f48cac7283956e991d1fed2b4412e19d6
Felipe Bucherenick <felipebucherenick@gmail.com>
2022-03-07 16:33:52 -0300

    Little fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routerApi = require('./routes');
 const app = express();
+const routerApi = require('./routes');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
