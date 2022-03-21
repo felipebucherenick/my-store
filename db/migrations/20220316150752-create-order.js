99886a51ae4a19a3fad0e3a9eaaf51614a4f784c
Felipe Bucherenick <felipebucherenick@gmail.com>
2022-03-21 18:17:01 -0300

    fix migration create-order

--- db/migrations/20220316150752-create-order.js
+++ db/migrations/20220316150752-create-order.js
@@ -1,9 +1,41 @@
 'use strict';
-const { ORDER_TABLE, OrderSchema } = require('../models/orderModel');
+const { ORDER_TABLE } = require('../models/orderModel');
+const { DataTypes, Sequelize } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customerModel');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+      },
+      customerId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        field: 'updated_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
   async down(queryInterface) {
