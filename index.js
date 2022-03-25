86d28997ab0f9d05ceb1f3ac37eb282c2813881e
Felipe Bucherenick <felipebucherenick@gmail.com>
2022-03-25 19:01:03 -0300

    fix CORS problem

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const {
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = [
+  'http://localhost:8080',
+  'https://myapp.co',
+  'http://localhost:3000',
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
